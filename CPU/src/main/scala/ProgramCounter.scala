275e21c1ebe3facc598685629d63ef8fb1db95af
slayervictor <victor.r2002@gmail.com>
2025-10-08 15:34:42 +0200

    ProgramCounter T2 Done

--- CPU/src/main/scala/ProgramCounter.scala
+++ CPU/src/main/scala/ProgramCounter.scala
@@ -8,8 +8,18 @@ class ProgramCounter extends Module {
     val run = Input(Bool())
     val programCounterJump = Input(UInt(16.W))
     val programCounter = Output(UInt(16.W))
+    val regFile = Mem(32, UInt(32.W))
   })
-
+  
   //Implement this module here (respect the provided interface, since it used by the tester)
+  val counterReg = RegInit(0.U(16.W))
+
+  when(io.jump) {
+    counterReg := io.programCounterJump
+  } .elsewhen(io.run && !io.stop) {
+    counterReg := counterReg + 1.U
+  }
+
+  io.programCounter := counterReg
 
 }
