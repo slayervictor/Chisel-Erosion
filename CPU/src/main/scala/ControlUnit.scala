c4e2fd7a5cd34fc3dbca92f80e76dbff600dd409
Victor Reynolds <victor.r2002@gmail.com>
2025-10-29 14:56:06 +0100

    RegisterFile

--- CPU/src/main/scala/ControlUnit.scala
+++ CPU/src/main/scala/ControlUnit.scala
@@ -4,6 +4,8 @@ import chisel3.util._
 class ControlUnit extends Module {
   val io = IO(new Bundle {
     //Define the module interface here (inputs/outputs)
+    val instruction = Input(UInt(32.W))
+    val opcode = Output(UInt(7.W))
   })
 
   //Implement this module here
--- CPU/src/main/scala/RegisterFile.scala
+++ CPU/src/main/scala/RegisterFile.scala
@@ -3,9 +3,24 @@ import chisel3.util._
 
 class RegisterFile extends Module {
   val io = IO(new Bundle {
-    //Define the module interface here (inputs/outputs)
+    // 2^4 = 16
+    val aSel = Input(UInt(4.W))
+    val bSel = Input(UInt(4.W))
+    val writeData = Input(UInt(32.W))
+    val writeSel = Input(UInt(4.W))
+    val writeEnable = Input(Bool())
+
+    val a = Output(UInt(32.W))
+    val b = Output(UInt(32.W))
   })
 
-  //Implement this module here
+  val registers = Reg(Vec(16, UInt(32.W)))
+
+  when(io.writeEnable) {
+    regs(io.writeSel) := io.writeData
+  }
+
+  io.a := regs(io.aSel)
+  io.b := regs(io.bSel)
 
 }
