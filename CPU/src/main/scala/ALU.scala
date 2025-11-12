5e5b6ad2791bb8c5e3b5ae8df1150e8796f4d8f6
Sebastian-Francis-Taylor <me@sebastian-taylor.com>
2025-11-12 15:36:06 +0100

    ALU is now done

--- CPU/src/main/scala/ALU.scala
+++ CPU/src/main/scala/ALU.scala
@@ -1,17 +1,30 @@
 import chisel3._
 import chisel3.util._
 
-// ALU just needs to add and give the result as well as check if zero
 class ALU extends Module {
-  val io = IO(new Bundle {
-    val aSel = Input(UInt(16.W))
-    val bSel = Input(UInt(16.W))
-    val func = Input(Bool())
-    val result = Output(UInt(16.W))
-  })
+    val io = IO(new Bundle {
+        val aSel   = Input(UInt(16.W))
+        val bSel   = Input(UInt(16.W))
+        val func   = Input(UInt(3.W))
+        val result = Output(UInt(16.W))
+    })
 
-  val add = io.aSel.asSInt + io.bSel.asSInt
-  val isZero = (io.aSel === 0.U)
+    val add    = io.aSel.asSInt + io.bSel.asSInt
+    val mult   = io.aSel.asSInt * io.bSel.asSInt
+    val or     = io.aSel.asSInt | io.bSel.asSInt
+    val and    = io.aSel.asSInt & io.bSel.asSInt
+    val not    = ~io.bSel.asSInt
+    val isZero = (io.aSel === 0.U)
 
-  io.result := Mux(io.func, isZero.asUInt, add.asUInt)
+    // MuxLookup or switch to select operation
+    io.result := MuxLookup(io.func, 0.U)(
+      Seq(
+        0.U -> add.asUInt,
+        1.U -> mult.asUInt,
+        2.U -> or.asUInt,
+        3.U -> and.asUInt,
+        4.U -> not.asUInt,
+        5.U -> isZero.asUInt
+      )
+    )
 }
