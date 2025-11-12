019860b5153a0b4a740892b4e21dbaa45d7861c0
Victor Reynolds <victor.r2002@gmail.com>
2025-11-12 14:13:11 +0100

    ALUve you

--- CPU/src/main/scala/ALU.scala
+++ CPU/src/main/scala/ALU.scala
@@ -3,9 +3,14 @@ import chisel3.util._
 
 class ALU extends Module {
   val io = IO(new Bundle {
-    // Define the module interface here (inputs/outputs)
+    val aSel = Input(Uint(16.W))
+    val bSel = Input(Uint(16.W))
+    val func = Input(Bool())
+    val result = Output(Uint(16.W))
   })
 
-  // Implement this module here
+  val add = io.aSel.asSInt() + io.bSel.asSInt()
+  val isZero = (io.aSel === 0.U)
 
+  io.result := Mux(io.func, isZero.asUInt(), add.asUInt())
 }
