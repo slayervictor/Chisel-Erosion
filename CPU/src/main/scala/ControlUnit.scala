8b840a64e7222bfb5ed1b20e88898ff65b46ac4f
Sebastian-Francis-Taylor <me@sebastian-taylor.com>
2025-11-12 15:54:51 +0100

    all tests run

--- CPU/src/main/scala/ControlUnit.scala
+++ CPU/src/main/scala/ControlUnit.scala
@@ -10,13 +10,21 @@ class ControlUnit extends Module {
     val reg2 = Output(UInt(4.W))
     val reg3 = Output(UInt(4.W))
     val imm = Output(UInt(16.W))
+    val nop = Output(Bool())
     val li = Output(Bool())
     val lb = Output(Bool())
     val sb = Output(Bool())
     val branch = Output(Bool())
     val add = Output(Bool())
     val addi = Output(Bool())
+    val mul = Output(Bool())
+    val or = Output(Bool())
+    val and = Output(Bool())
+    val not = Output(Bool())
     val j = Output(Bool())
+    val jeq = Output(Bool())
+    val jlt = Output(Bool())
+    val jgt = Output(Bool())
     val exit = Output(Bool())
   })
 
@@ -32,16 +40,36 @@ class ControlUnit extends Module {
   io.reg2 := regB
   io.reg3 := regC
   io.imm := imm
+  io.nop := false.B
   io.add := false.B
   io.addi := false.B
+  io.mul := false.B
+  io.or := false.B
+  io.and := false.B
+  io.not := false.B
   io.li := false.B
   io.lb := false.B
   io.sb := false.B
   io.branch := false.B
   io.j := false.B
+  io.jeq := false.B
+  io.jlt := false.B
+  io.jgt := false.B
   io.exit := false.B
 
   switch(opcode) {
+    // NOP
+    is("b0000".U) {
+      io.nop := true.B
+    }
+    // Add
+    is("b0001".U) {
+      io.add := true.B
+    }
+    // Add Immediate
+    is("b0010".U) {
+      io.addi := true.B
+    }
     // Load immediate
     is("b0011".U) {
       io.li := true.B
@@ -58,14 +86,6 @@ class ControlUnit extends Module {
     is("b0110".U) {
       io.branch := true.B
     }
-    // Add
-    is("b0001".U) {
-      io.add := true.B
-    }
-    // Add Immediate
-    is("b0010".U) {
-      io.addi := true.B
-    }
     // Jump
     is("b0111".U) {
       io.j := true.B
@@ -73,6 +93,34 @@ class ControlUnit extends Module {
     // Exit
     is("b1000".U) {
       io.exit := true.B
+    }
+    // Multiply
+    is("b1001".U) {
+      io.mul := true.B
+    }
+    // OR
+    is("b1010".U) {
+      io.or := true.B
+    }
+    // AND
+    is("b1011".U) {
+      io.and := true.B
+    }
+    // NOT
+    is("b1100".U) {
+      io.not := true.B
+    }
+    // Jump if equal
+    is("b1101".U) {
+      io.jeq := true.B
+    }
+    // Jump if less than
+    is("b1110".U) {
+      io.jlt := true.B
+    }
+    // Jump if greater than
+    is("b1111".U) {
+      io.jgt := true.B
     }
   }
 }
