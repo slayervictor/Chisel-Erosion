9ef2673b59cf1a15c0944a7d407be9d84aa8d9c6
Sebastian-Francis-Taylor <me@sebastian-taylor.com>
2025-11-16 19:12:43 +0100

    cleaned up for final handover

--- Erosion/erosion.c
+++ Erosion/erosion.c
@@ -4,101 +4,84 @@
 #define W 20
 #define H 20
 
-int main()
-{
+int main() {
 
-    char input_chars[H][50] = {
-        ". . . . . . . . . . . . . . . . . . . .",
-        ". . . . . . . . . . . . . . . . . . . .",
-        ". . . . . . . . . . . . . . . . . . . .",
-        ". . . . . . . . . . . . . . . . . . . .",
-        ". . . . . . . . . . . @ . . . .",
-        ". . . . @ . . . . . . . . @ @ @ . . .",
-        ". . . @ @ @ . . . . . . @ @ @ @ @ . .",
-        ". . @ @ @ @ @ . . . . . . @ @ @ . . .",
-        ". . @ @ @ @ @ @ . . . . . . @ . . . .",
-        ". . @ @ @ @ @ @ . . . . . . . . . . .",
-        ". . @ @ @ @ @ @ . . . . . . . . . . .",
-        ". . . @ @ @ @ . . . . . . . . . . . .",
-        ". . . . @ @ . . . . . . . . . . . . .",
-        ". . . . @ . . . . . . @ @ @ . . . . .",
-        ". . . . . . . . . . . @ @ @ . . . . .",
-        ". . . . . . . . . . . @ @ @ . . . . .",
-        ". . . . . . . . . . . @ @ @ . . . . .",
-        ". . . . . . . . . . . . . . . . . . .",
-        ". . . . . . . . . . . . . . . . . . .",
-        ". . . . . . . . . . . . . . . . . . ."};
+  char input_chars[H][50] = {". . . . . . . . . . . . . . . . . . . .",
+                             ". . . . . . . . . . . . . . . . . . . .",
+                             ". . . . . . . . . . . . . . . . . . . .",
+                             ". . . . . . . . . . . . . . . . . . . .",
+                             ". . . . . . . . . . . @ . . . .",
+                             ". . . . @ . . . . . . . . @ @ @ . . .",
+                             ". . . @ @ @ . . . . . . @ @ @ @ @ . .",
+                             ". . @ @ @ @ @ . . . . . . @ @ @ . . .",
+                             ". . @ @ @ @ @ @ . . . . . . @ . . . .",
+                             ". . @ @ @ @ @ @ . . . . . . . . . . .",
+                             ". . @ @ @ @ @ @ . . . . . . . . . . .",
+                             ". . . @ @ @ @ . . . . . . . . . . . .",
+                             ". . . . @ @ . . . . . . . . . . . . .",
+                             ". . . . @ . . . . . . @ @ @ . . . . .",
+                             ". . . . . . . . . . . @ @ @ . . . . .",
+                             ". . . . . . . . . . . @ @ @ . . . . .",
+                             ". . . . . . . . . . . @ @ @ . . . . .",
+                             ". . . . . . . . . . . . . . . . . . .",
+                             ". . . . . . . . . . . . . . . . . . .",
+                             ". . . . . . . . . . . . . . . . . . ."};
 
-    unsigned char in_image[H][W];
-    unsigned char out_image[H][W];
+  unsigned char in_image[H][W];
+  unsigned char out_image[H][W];
 
-    // Convert ASCII input to image data
-    for (int y = 0; y < H; y++)
-    {
-        int pos = 0;
-        for (int x = 0; x < W; x++)
-        {
-            char c = input_chars[y][pos];
-            pos += 2;
-            in_image[y][x] = (c == '@') ? 255 : 0;
-        }
+  // Convert ASCII input to image data
+  for (int y = 0; y < H; y++) {
+    int pos = 0;
+    for (int x = 0; x < W; x++) {
+      char c = input_chars[y][pos];
+      pos += 2;
+      in_image[y][x] = (c == '@') ? 255 : 0;
     }
+  }
 
-    // High-precision timer setup
-    LARGE_INTEGER freq, t1, t2;
-    QueryPerformanceFrequency(&freq);
-    QueryPerformanceCounter(&t1);
+  // High-precision timer setup
+  LARGE_INTEGER freq, t1, t2;
+  QueryPerformanceFrequency(&freq);
+  QueryPerformanceCounter(&t1);
 
-    // ---- Perform erosion ----
-    for (int y = 0; y < H; y++)
-    {
-        for (int x = 0; x < W; x++)
-        {
+  // ---- Perform erosion ----
+  for (int y = 0; y < H; y++) {
+    for (int x = 0; x < W; x++) {
 
-            if (x == 0 || y == 0 || x == W - 1 || y == H - 1)
-            {
-                out_image[y][x] = 0;
-                continue;
-            }
+      if (x == 0 || y == 0 || x == W - 1 || y == H - 1) {
+        out_image[y][x] = 0;
+        continue;
+      }
 
-            if (in_image[y][x] == 0)
-            {
-                out_image[y][x] = 0;
-            }
-            else
-            {
-                if (in_image[y - 1][x] == 0 ||
-                    in_image[y + 1][x] == 0 ||
-                    in_image[y][x - 1] == 0 ||
-                    in_image[y][x + 1] == 0)
-                {
-                    out_image[y][x] = 0;
-                }
-                else
-                {
-                    out_image[y][x] = 255;
-                }
-            }
+      if (in_image[y][x] == 0) {
+        out_image[y][x] = 0;
+      } else {
+        if (in_image[y - 1][x] == 0 || in_image[y + 1][x] == 0 ||
+            in_image[y][x - 1] == 0 || in_image[y][x + 1] == 0) {
+          out_image[y][x] = 0;
+        } else {
+          out_image[y][x] = 255;
         }
+      }
     }
+  }
 
-    QueryPerformanceCounter(&t2);
+  QueryPerformanceCounter(&t2);
 
-    // Time in microseconds
-    double elapsed_us =
-        (double)(t2.QuadPart - t1.QuadPart) * 1000000.0 / freq.QuadPart;
+  // Time in microseconds
+  double elapsed_us =
+      (double)(t2.QuadPart - t1.QuadPart) * 1000000.0 / freq.QuadPart;
 
-    printf("Execution time: %.3f microseconds\n\n", elapsed_us);
+  printf("Execution time: %.3f microseconds\n\n", elapsed_us);
 
-    // Print eroded output image
-    for (int y = 0; y < H; y++)
-    {
-        for (int x = 0; x < W; x++)
-        {
-            printf("%c ", out_image[y][x] ? '@' : '.');
-        }
-        printf("\n");
+  // Print eroded output image
+  for (int y = 0; y < H; y++) {
+    for (int x = 0; x < W; x++) {
+      printf("%c ", out_image[y][x] ? '@' : '.');
     }
+    printf("\n");
+  }
 
-    return 0;
+  return 0;
 }
--- README.md
+++ README.md
@@ -1,7 +1,26 @@
 # Chisel-Erosion
 ## About
 
-This is for assignment 2 of the course *Computer Systems (02132)* at the Technical University of Denmark. The CPU designed in this project is designed from scratch with some inspiration from the RISC-V architecutre.
+This is for assignment 2 of the course *Computer Systems (02132)* at the Technical University of Denmark. The CPU designed in this project is designed from scratch with some inspiration from the RISC-V architecture.
+
+## Architecture
+
+### Registers
+- **8 general-purpose registers** (R0-R7)
+- **32-bit wide** registers
+
+### Data Path
+- **Instruction width**: 32 bits
+- **ALU operations**: 16-bit data path
+
+### Instruction Format
+```
+Bits [31:16] - Immediate value (16-bit)
+Bits [15:12] - Register 3 (4-bit)
+Bits [11:8]  - Register 2 (4-bit)
+Bits [7:4]   - Register 1 (4-bit)
+Bits [3:0]   - Opcode (4-bit)
+```
 
 ## Erosion
 To run the Erosion algorithm, enter the CPU directory via `cd CPU` and run the CPUTopTester with the following command
@@ -17,30 +36,33 @@ sbt test
 This will also run the CPUTopTester which runs the Erosion program.
 
 ## Software
-The Erosion program can be found in the `Erosion` directory, since the code is assembled by hand, there is no streamlined way of writing software for this architecutre.
+The Erosion program can be found in the `Erosion` directory, since the code is assembled by hand, there is no streamlined way of writing software for this architecture.
 
-## limitations
-The CPU Supports basic logical and arithmetic operations, but it is general purpose, so any software should run, assuming that the assembly is written with the limited scope in mind.
+## Limitations
+The CPU has the following constraints:
+- **8 registers only** (R0-R7)
+- **No subtraction instruction** (use ADDI with negative immediate values)
+- **No division instruction**
+- **16-bit immediate values** (range: -32768 to 32767)
+- **No floating-point support**
+- Basic logical and arithmetic operations only
 
 ## Instruction Set
-| Opcode | Binary | Instruction | Description |
-|--------|--------|-------------|-------------|
-| 0 | 0000 | NOP | No operation |
-| 1 | 0001 | ADD | R1 = R2 + R3 |
-| 2 | 0010 | ADDI | R1 = R2 + imm |
-| 3 | 0011 | LI | R1 = imm (Load Immediate) |
-| 4 | 0100 | LB | R1 = memory[R2] (Load Byte) |
-| 5 | 0101 | SB | memory[R2] = R1 (Store Byte) |
-| 6 | 0110 | BRANCH | Branch if R2 == 0 |
-| 7 | 0111 | J | Jump to address in imm |
-| 8 | 1000 | EXIT | Terminate execution |
-| 9 | 1001 | MUL | R1 = R2 * R3 |
-| 10 | 1010 | OR | R1 = R2 OR R3 |
-| 11 | 1011 | AND | R1 = R2 AND R3 |
-| 12 | 1100 | NOT | R1 = NOT(R2) |
-| 13 | 1101 | JEQ | Jump if R2 == R3 |
-| 14 | 1110 | JLT | Jump if R2 < R3 |
-| 15 | 1111 | JGT | Jump if R2 > R3 |
+| Opcode | Binary | Instruction | Operation | Example |
+|--------|--------|-------------|-----------|---------|
+| 0 | 0000 | NOP | No operation | `0x00000000` |
+| 1 | 0001 | ADD | reg1 = reg2 + reg3 | `0x00012301` (R3=R1+R2) |
+| 2 | 0010 | ADDI | reg1 = reg2 + imm | `0x000A1002` (R1=R1+10) |
+| 3 | 0011 | LI | reg1 = imm | `0x00FF1003` (R1=255) |
+| 4 | 0100 | LB | reg1 = memory[reg2] | `0x00002104` (R1=mem[R2]) |
+| 5 | 0101 | SB | memory[reg2] = reg1 | `0x00002105` (mem[R2]=R1) |
+| 6 | 0110 | BRANCH | if reg1 == 0, PC = imm | `0x00050026` (if R2==0, jump to 5) |
+| 7 | 0111 | J | PC = imm | `0x000A0007` (jump to addr 10) |
+| 8 | 1000 | EXIT | Halt execution | `0x00000008` |
+| 9 | 1001 | MUL | reg1 = reg2 * reg3 | `0x00012309` (R3=R1*R2) |
+| 10 | 1010 | OR | reg1 = reg2 OR reg3 | `0x0001230A` (R3=R1\|R2) |
+| 11 | 1011 | AND | reg1 = reg2 AND reg3 | `0x0001230B` (R3=R1&R2) |
+| 12 | 1100 | NOT | reg1 = NOT(reg2) | `0x0000210C` (R1=~R2) |
 
 ## Authors
 - Victor Reynolds - [@slayervictor](https://github.com/slayervictor)
